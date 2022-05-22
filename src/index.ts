8fdad3acabd64a1cbb048a432a0ef9bc08b9c8ec
Gabriela <104107313+mgbgabriela@users.noreply.github.com>
2022-05-21 21:09:57 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+/*Ejercicio: Potencias
+• Realice un programa que devuelva la potencia de un número.
+• La base y el exponente deben ser ingresados por teclado.
+• Sólo deben admitirse exponentes mayores o iguales a cero.
+• Recuerde que el resultado de un numero elevado a 0 es 1.
+• Separe la lógica de calcular la potencia utilizando métodos.
+35
+34=3x3x3x3=81
+Base Resultado*/
+
+
 let dato1 = document.getElementById("dato1");
 let dato2 = document.getElementById("dato2");
 let rotulo1 = document.getElementById("rotulo1");
